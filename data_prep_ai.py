a08d636957407cdf1dc228486f2f2c0dbb24b801
lixavi <121849896+lixavi@users.noreply.github.com>
2021-04-19 20:28:40 +0530

    updated

---
+++ data_prep_ai.py
@@ -0,0 +1,28 @@
+# data_prep_ai.py
+
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+def preprocess_data(data):
+    # Your data preprocessing logic here
+    pass
+
+def scale_features(data):
+    scaler = StandardScaler()
+    scaled_data = scaler.fit_transform(data)
+    return scaled_data
+
+
+# main.py
+
+#import data_prep_ai
+
+#def main():
+    # Your main program logic here
+    #pass
+
+#if __name__ == "__main__":
+    #main()
+    
+    
+    
